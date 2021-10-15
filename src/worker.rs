175c761ca96d092645dc9127a1b15141bcfec809
Arindam Das <dasarindam.mails@gmail.com>
2021-10-15 18:08:38 +0530

    Removed redundant mutex wrapper for receiver, fixed API leak by removing steal_job()

--- src/worker.rs
+++ src/worker.rs
@@ -3,14 +3,11 @@
 use std::{
     error::Error,
     fmt::{Debug, Display},
-    sync::{
-        mpsc::{Receiver, Sender},
-        Arc, Mutex,
-    },
+    sync::mpsc::{Receiver, Sender},
     thread::{self, JoinHandle},
 };
 
-use crate::{job::Job, message::Message};
+use crate::message::Message;
 
 /// Worker represents a worker thread capable for receiving
 /// and servicing jobs.
@@ -22,7 +19,6 @@ where
     uid: u64,
 
     disp_q: Sender<Message<Req, Res>>,
-    job_source: Arc<Mutex<Receiver<Message<Req, Res>>>>,
 
     worker: Option<JoinHandle<Result<(), WorkerError>>>,
     pending: usize,
@@ -77,14 +73,10 @@ where
         done: Sender<u64>,
         uid: u64,
     ) -> Self {
-        let job_source = Arc::new(Mutex::new(job_source));
-        let worker = Self::worker_thread(Arc::clone(&job_source), done, uid);
-
         Worker {
             uid,
             disp_q,
-            job_source,
-            worker: Some(worker),
+            worker: Some(Self::worker_thread(job_source, done, uid)),
             pending: 0,
         }
     }
@@ -104,12 +96,14 @@ where
     /// ```
     #[inline]
     pub fn worker_thread(
-        jobs: Arc<Mutex<Receiver<Message<Req, Res>>>>,
+        jobs: Receiver<Message<Req, Res>>,
         done: Sender<u64>,
         uid: u64,
     ) -> JoinHandle<Result<(), WorkerError>> {
         thread::spawn(move || -> Result<(), WorkerError> {
-            while let Ok(Message::Request(job)) = jobs.lock().unwrap().recv() {
+            let job_source = jobs;
+
+            while let Ok(Message::Request(job)) = job_source.recv() {
                 job.resp_with_result()
                     .or(Err(WorkerError::ResultResponseFailed))?;
 
@@ -147,15 +141,6 @@ where
     pub fn dec_load(&mut self) {
         self.pending -= 1;
     }
-
-    /// Steals a message from this worker and returns the job within
-    /// that message.
-    pub fn steal_job(&mut self) -> Option<Job<Req, Res>> {
-        return match self.job_source.lock().unwrap().recv().ok() {
-            Some(Message::Request(job)) => Some(job),
-            _ => None,
-        };
-    }
 }
 
 impl<Req, Res> Drop for Worker<Req, Res>
@@ -171,7 +156,8 @@ where
 
 #[cfg(test)]
 mod tests {
-    use super::{Job, Message, Worker};
+    use super::{Message, Worker};
+    use crate::job::Job;
     use std::sync::mpsc::channel;
 
     #[test]
